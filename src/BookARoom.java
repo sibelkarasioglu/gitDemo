608cb67bf85551c00e269aa030bdef6de64786a6
sibelkarasioglu <sibelkarasioglu@hotmail.com>
2020-08-01 15:36:49 -0400

    add BookARoom.java

---
+++ src/BookARoom.java
@@ -0,0 +1,12 @@
+public class BookARoom {
+    public static void main(String[] args) {
+        System.out.println("*****");
+        for (int j = 0; j < 5; j++){
+            System.out.println("*   *");
+        }
+        System.out.println("*****");
+    }
+
+}
+
+
--- src/Happy20.java
+++ src/Happy20.java
@@ -12,5 +12,10 @@ public class Happy20 {
 
 
 
+
+
+        //add lines
+
+
     }
 }
