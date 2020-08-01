a2f9b0f3e6567d07493375375599ad670a27c439
sibelkarasioglu <68888878+sibelkarasioglu@users.noreply.github.com>
2020-08-01 16:02:04 -0400

    Update BookARoom.java

--- src/BookARoom.java
+++ src/BookARoom.java
@@ -1,10 +1,7 @@
 public class BookARoom {
     public static void main(String[] args) {
-        System.out.println("*****");
-        for (int j = 0; j < 5; j++){
-            System.out.println("*   *");
-        }
-        System.out.println("*****");
+       //add new line
+            
     }
 
 }
